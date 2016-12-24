64646959f8556e637c43a69f724b64ba78e4e6d4
Ben Denzer <denzer.ben@gmail.com>
2016-12-23 22:08:30 -0800

    initialstate showing in HomePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Home = (props) => {
+    console.log(props.counter);
     return (
-        <div><h1>Home</h1></div>
+        <div>
+            <h1>Home</h1>
+            <h2>{props.counter}</h2>
+        </div>
     );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        counter: state.counter,
+    }
+};
+
+export default connect(mapStateToProps)(Home);
