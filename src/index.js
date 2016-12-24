c9262eeda87dcee8b4b8fee6b6ab5bbf513a31eb
Ben Denzer <denzer.ben@gmail.com>
2016-12-23 21:17:22 -0800

    removed un-needed files

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import Nav from './components/shared/Nav';
 import Routes from './components/shared/Routes';
 
 const App = () => (
-  // 2. render a `Router`, it will listen to the url changes
-  //    and make the location available to other components
-  //    automatically
-
   <BrowserRouter>
     <div>
       <Nav />
       <hr/>
-      <Routes />
+      <TopLevelRoutes />
     </div>
   </BrowserRouter>
 )
 
-render(<App/>, document.querySelector('#root'));
+render(<App/>, document.getElementById('root'));
